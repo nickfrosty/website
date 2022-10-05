d8acb0c47029baad49778ec188763445dea409c1
nickfrosty <nfrostbutter@gmail.com>
2022-10-04 20:38:40 -0400

    fix: project page spacing

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -50,7 +50,9 @@ export default function ArticlesIndex({ projects, featured }) {
         </section>
       )}
 
-      <hr />
+      <section>
+        <hr />
+      </section>
 
       <section className="mx-auto mb-12 max-w-2xl text-center">
         <h1 className="mb-5 text-4xl font-bold">
