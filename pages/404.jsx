6bcd6024c7c7ba212194d5889113791d7fbc1e30
nick-crossmint <nick@paella.dev>
2022-10-13 15:27:24 -0400

    feat: added 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,33 @@
+import Layout from "~/layouts/default";
+// import { basicMeta } from "~/utils/seoMetaData";
+
+// construct the meta data for the page
+// const metaData = basicMeta({
+const metaData = {
+  title: "Nope, couldn't find that page...",
+  titleTemplate: "%s",
+  description: "I swear this never happens.",
+};
+
+export default function ErrorPage() {
+  return (
+    <Layout seo={metaData} className="my-20 text-center md:space-y-16">
+      <h1 className="text-5xl font-bold md:block">
+        Well, this is embarrassing...
+      </h1>
+
+      <main className="mx-auto space-y-12 max-w-xl text-2xl">
+        <p>
+          You found a page that does not exist, or I moved it.
+          <br />
+          Or am hiding it from you?? 🙃
+        </p>
+
+        <p>
+          You probably shouldn't try to look for it,
+          <br /> just move on.
+        </p>
+      </main>
+    </Layout>
+  );
+}
