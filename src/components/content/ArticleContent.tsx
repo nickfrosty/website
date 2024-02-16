56d0ec0a076dab3df4ccaf6486810563df92f747
Nick <nick@frostbutter.com>
2024-02-15 22:23:29 -0500

    chore: update all packages

--- src/components/content/ArticleContent.tsx
+++ src/components/content/ArticleContent.tsx
@@ -3,8 +3,6 @@ import styles from "@/styles/article.module.css";
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import remarkGfm from "remark-gfm";
-import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
 type ComponentProps = {
   className?: string;
@@ -17,45 +15,9 @@ export function ArticleContent({
 }: ComponentProps) {
   return (
     <article className={`${styles.article} ${className}`}>
-      <ReactMarkdown
-        rehypePlugins={[rehypeRaw, remarkGfm]}
-        components={{ code: CodeBlock }}
-      >
+      <ReactMarkdown rehypePlugins={[rehypeRaw, remarkGfm]}>
         {content}
       </ReactMarkdown>
     </article>
   );
-}
-
-type CodeBlockProps = { className?: string; inline?: boolean; children: any };
-
-/*
-  Define a custom reusable code block component
-*/
-function CodeBlock({ className, inline, children }: CodeBlockProps) {
-  // trim white space and extra lines at the end
-  if (Array.isArray(children)) {
-    for (let i = 0; i < children.length; i++) {
-      children[i] = children[i].trim();
-    }
-    // children[children.length - 1] = children[children.length - 1].trim();
-  } else if (typeof children === "string") children = children.trim();
-
-  // compute the `language`
-  let language = className?.slice("language-".length).toLowerCase() ?? "";
-
-  if (language === "sh") language = "bash";
-
-  if (inline) return <span className="inline-code">{children}</span>;
-  else
-    return (
-      <SyntaxHighlighter
-        className={className}
-        style={dracula}
-        language={language}
-        showLineNumbers={true}
-      >
-        {children}
-      </SyntaxHighlighter>
-    );
 }
