ed559f95052e51d0975a23a2810bf26144880bfd
Nick <nick@frostbutter.com>
2024-02-18 16:40:38 -0500

    feat: better default metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { Metadata } from "next";
-import { SITE } from "@/lib/config";
+import { SITE, SOCIAL } from "@/lib/config";
 
 import AppHeader from "@/components/core/AppHeader";
 import AppFooter from "@/components/core/AppFooter";
@@ -21,6 +21,25 @@ export const metadata: Metadata = {
   description:
     "Hi! I'm Nick, a full stack developer and submariner working on various " +
     "projects. In my free time I write software, technical articles, and build things.",
+  openGraph: {
+    siteName: SITE.name,
+    type: "website",
+    images: [
+      {
+        url: "/img/nick.jpg",
+        width: 256,
+        height: 256,
+        alt: SITE.name,
+      },
+    ],
+  },
+  twitter: {
+    site: `@${SOCIAL.twitter}`,
+    creator: `@${SOCIAL.twitter}`,
+    card: "summary",
+    // card: "summary_large_image",
+  },
+  category: "technology",
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
