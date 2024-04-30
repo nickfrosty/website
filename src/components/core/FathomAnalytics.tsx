158b2b9d682ff945f26284dacfe9ad75213c0c6f
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-04-30 17:03:29 -0400

    refactor: fathom

--- src/components/core/FathomAnalytics.tsx
+++ src/components/core/FathomAnalytics.tsx
@@ -9,15 +9,11 @@ function TrackPageView() {
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    if (!process?.env?.NEXT_PUBLIC_FATHOM_ANALYTICS_SITE_ID) {
-      return console.warn("Missing Fathom analytics key");
-    }
-
     if (process?.env?.NODE_ENV !== "production") return;
 
-    load(process.env.NEXT_PUBLIC_FATHOM_ANALYTICS_SITE_ID, {
+    load("TPWTJRRN", {
       auto: false,
-      includedDomains: [SITE.domain, `www.${SITE.domain}`],
+      // includedDomains: [SITE.domain, `www.${SITE.domain}`],
     });
   }, []);
 
