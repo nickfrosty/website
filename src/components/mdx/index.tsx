b84120b38b9e5d7ae36568c427dc5c29ee1dfbd7
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2026-02-01 11:52:58 -0500

    refactor: callout

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -1,19 +1,11 @@
-import React, { Children, useMemo, type ComponentProps } from "react";
+import React, { Children, type ComponentProps } from "react";
 
 import Link from "next/link";
 
-import {
-  BoltIcon,
-  ExclamationTriangleIcon,
-  InformationCircleIcon,
-  SparklesIcon,
-  StarIcon,
-} from "@heroicons/react/24/outline";
 import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
 
 import { REGEX_CONTENT_DIR_LINK, slugify } from "@@/utils/helpers";
-
-import { CalloutProps } from "./rehypeConfig";
+import { Callout } from "@/components/ui";
 
 import type { MdxContent } from "@fumadocs/mdx-remote/client";
 import type { MDXComponents } from "mdx/types";
@@ -98,53 +90,6 @@ function CustomImage({ ref, ...props }: ComponentProps<"img">) {
   return <img src={src} {...props} />;
 }
 
-function Callout(props: ComponentProps<"div"> & CalloutProps) {
-  const IconToUse = useMemo(() => {
-    switch (props.type) {
-      case "warn":
-      case "warning":
-      case "caution":
-      case "yellow":
-      case "red":
-      case "error":
-        return ExclamationTriangleIcon;
-      case "green":
-      case "success":
-        return StarIcon;
-      case "blockquote":
-      case "sparkles":
-        return SparklesIcon;
-      case "blue":
-      case "note":
-      case "pro":
-        return BoltIcon;
-      default:
-        return InformationCircleIcon;
-    }
-  }, []);
-
-  return (
-    <div
-      className={`callout ${(props?.type as string)?.toLowerCase() || "note"} ${
-        props.className ? props.className : ""
-      }`}
-    >
-      <div className="callout-icon">
-        <IconToUse className="" />
-      </div>
-      <div {...props} className={"callout-content"}>
-        {!!props.title && <h5 className="callout-title">{props.title}</h5>}
-        {props.children}
-      </div>
-    </div>
-  );
-}
-
-function Blockquote({ ref, ...props }: ComponentProps<"blockquote">) {
-  // @ts-ignore
-  return <Callout {...props} type="blockquote" className="indigo" />;
-}
-
 function Line(props: ComponentProps<"hr">) {
   return (
     <div className="">
@@ -155,13 +100,14 @@ function Line(props: ComponentProps<"hr">) {
 
 export const defaultMdxComponents: MDXComponents = {
   // convert h1 to h2 since the layout will ship the h1
-  h1: (props: any) => <AnchorHeading as="h2" {...props} />,
-  h2: (props: any) => <AnchorHeading as="h2" {...props} />,
-  h3: (props: any) => <AnchorHeading as="h3" {...props} />,
-  h4: (props: any) => <AnchorHeading as="h4" {...props} />,
-  h5: (props: any) => <AnchorHeading as="h5" {...props} />,
-  h6: (props: any) => <AnchorHeading as="h6" {...props} />,
+  h1: props => <AnchorHeading as="h2" {...props} />,
+  h2: props => <AnchorHeading as="h2" {...props} />,
+  h3: props => <AnchorHeading as="h3" {...props} />,
+  h4: props => <AnchorHeading as="h4" {...props} />,
+  h5: props => <AnchorHeading as="h5" {...props} />,
+  h6: props => <AnchorHeading as="h6" {...props} />,
   hr: Line,
+  blockquote: props => <Callout type="blockquote">{props.children}</Callout>,
   pre: ({ children, ...props }) => (
     <CodeBlock {...props}>
       <Pre>{children}</Pre>
@@ -170,7 +116,6 @@ export const defaultMdxComponents: MDXComponents = {
   a: CustomLink,
   Callout: Callout,
   img: CustomImage,
-  blockquote: Blockquote,
 };
 
 export function RenderMDX({ body: MDXBody }: { body: MdxContent }) {
